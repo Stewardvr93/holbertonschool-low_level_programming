cae5b0337b5ac8e5169a042c269c68eccdd97e80
Stewardvr <stewardvaldez@gmail.com>
2022-02-10 16:40:19 -0500

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,18 +7,6 @@
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-}
-#include <stdio.h>
-/**
- * main - 4-put
- *
- * Description:
- * Return:  0 ok
- */
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
