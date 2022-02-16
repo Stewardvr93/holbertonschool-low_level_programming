357799b6c894195471d0d50f91f42654969c7b49
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-02-16 18:30:52 -0500

    1-main.c

--- 0x03-debugging/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-#include <stdio.h>
-int positive_or_negative(int);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-#endif
