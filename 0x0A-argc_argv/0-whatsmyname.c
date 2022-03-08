80977ca0bec8ef36d498eda69d599a210a5c7a6d
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-03-08 12:28:11 -0500

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* main - aaa
+* @argc: argument count inside argument value
+* @argv: argument value (array)
+*
+* Return: aaa
+*/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*main - aaa
+*@argc: argument count
+*@argv: argument variable
+*Return: aaa
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
