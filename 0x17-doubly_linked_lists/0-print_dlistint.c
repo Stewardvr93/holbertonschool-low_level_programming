c2931facabec03b37c8b394e53b953a6a0a1b7ec
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-05-13 10:17:00 -0500

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,17 @@
 #include "lists.h"
-
 /**
- * print_dlistint - function that prints all the elements of a dlistint_t list
- * @h: start of the linked list
- * Return: number of nodes
+ * print_dlistint - Imprimo todos los elementos de h.
+ * @h: Puntero a la lista.
+ * Return: Numero de nodos
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  int count = 0;
+	int nodos = 0;
 
-  while (h)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-      count++;
-    }
-  return (count);
+	for (; h; nodos++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodos);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,10 +1,11 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -12,12 +13,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * 
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -30,4 +32,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H */
+#endif
