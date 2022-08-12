13b5203ee6ade19d0ceb107a55717a3a648e3876
Stewardvr93 <91085277+Stewardvr93@users.noreply.github.com>
2022-08-12 14:32:28 -0500

    Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,71 +1,44 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
- * binary_search - searches for a value in an array of
- * integers using the Binary search algorithm
- * @array: array to search the value in
+ * binary_search - search the index localisation for the value
+ * @array: array
  * @size: size of the array
- * @value: value to look for
- *
- * Return: the index of the found value,
- * or -1 if not found
+ * @value: the value for find
+ * Return: mid or -1
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	if (!array || size == 0)
-		return (-1);
+	size_t mid = size / 2;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t index;
 
-	return (help_binary(array, value, 0, size - 1));
-}
-
-/**
- * help_binary - searches for a value in an array of
- * integers using recursion
- * @array: array to search the value in
- * @value: value to look for
- * @lo: index of the low bound
- * @hi: index of the high bound
- *
- * Return: the index of the found value,
- * or -1 if not found
- */
-int help_binary(int *array, int value, size_t lo, size_t hi)
-{
-	size_t mid;
-
-	array_print(array, lo, hi);
-	if (hi == lo && array[lo] != value)
-		return (-1);
-
-	mid = ((hi - lo) / 2) + lo;
-	if (array[mid] == value)
-		return (mid);
-	if (array[mid] < value)
-		return (help_binary(array, value, mid + 1, hi));
-	if (array[mid] > value)
-		return (help_binary(array, value, lo, mid - 1));
+	if (array == NULL)
 	return (-1);
-}
 
-/**
- * array_print - prints an array
- * @array: array to print
- * @lo: index of the low bound
- * @hi: index of the high bound
- */
-void array_print(int *array, size_t lo, size_t hi)
-{
-	size_t i;
-
-	printf("Searching in array: ");
-	for (i = lo; i <= hi; i++)
+	while (left <= right)
 	{
-		printf("%d", array[i]);
-		if (i < hi)
-			printf(", ");
+		printf("Searching in array: ");
+		for (index = left; index < right; index++)
+		{
+			printf("%d, ", array[index]);
+		}
+		printf("%d\n", array[index]);
+		mid = (left + right) / 2;
+		if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return (mid);
+		}
 	}
-	printf("\n");
+	return (-1);
 }
